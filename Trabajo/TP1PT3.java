85749ed1d068357fc332dc02da90c80ca1057826
Julio Geronimo Arabena <spotablecow28@hotmail.com>
2025-08-27 01:53:36 -0300

    Punto 3

---
+++ Trabajo/TP1PT3.java
@@ -0,0 +1,19 @@
+package Trabajo;
+public class TP1PT3 {
+    
+    public static void main (String[]args){
+    
+        int A=5;
+        int B=3;
+        int C=7;
+        int AUX=C;
+            if (A!=B && A!=C){
+                B=A;
+                C=A;
+                A=AUX;
+            }
+            System.out.println("El valor de A es de:"+A);
+            System.out.println("El valor de B es de:"+B);
+            System.out.println("El valor de C es de:"+C);
+    }
+}
